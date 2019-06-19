f2b0072f31fd904441a07095aefce0ef6823a2c0
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-19 10:25:55 -0600

    blog page stubbed out

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -1,16 +1,35 @@
-import React, {Component, useState, useEffect} from 'react'
+import React, { Component, useState, useEffect} from 'react'
 import axios from 'axios'
 import { List, Header, Segment, } from 'semantic-ui-react'
 
 
 class Blogs extends Component{
+  state = { blogs: [], }
+
+  componentDidMount(){
+    axios.get('/api/blogs')
+    .then(res =>{
+      this.setState({ blogs: res.data })
+    })
+  }
+  renderBlogs =()=>{
+    const { blogs } = this.state
+    return blogs.map(blog =>
+      <Segment key={ blog.id }>
+        <List.Header as = 'h3'>{ blog.title }</List.Header>
+        <List.Description>
+          { blog.body }
+        </List.Description>
+      </Segment>
+    )
+  }
   render(){
     return(
       <>
       <Header as = 'h1'>My Blogs</Header>
       <br/>
       <List>
-        
+        {this.renderBlogs()}
       </List>
       </>
     )
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { Header, } from 'semantic-ui-react';
 
 const Home = () => (
-  <Header as="h3" textAlign="center">Devise Auth App</Header>
+  <>
+  <br/>
+  <Header as="h3" textAlign="center">
+  Leggy Leggy Leggy Leggy 
+  </Header>
+  <Header as="h3" textAlign="center" >
+    Bloggy Bloggy Bloggy Bloggy
+  </Header>
+  </>
 )
 
 export default Home;
